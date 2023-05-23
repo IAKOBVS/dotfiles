7dea8ef5189e41f3cd11e630c459d47e2c50a2b3
James H <nus4xd@gmail.com>
2023-05-23 21:25:34 +0700

    new

--- .local/bin/scripts/uplibpath.pl
+++ .local/bin/scripts/uplibpath.pl
@@ -1,33 +1,34 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
+#DO NOT USE (BROKEN)
 
-# while (my $fnam = glob('*.c *.h *.cpp *.hpp')) {
-# 	open(my $fp, '+<', $fnam) or die "Can't open $fnam\n";
-# 	while (my $ln = <$fp>) {
-# 		# match file in #include file.cpp
-# 		if ($ln =~ !/^[ \t]*#[ \t]*include[ \t\/]{1,}"([^"]{1,})"/) {
-# 			next;
-# 		}
-# 		if (-e $1) {
-# 			next;
-# 		}
-# 		my $incl = $1;
-# 		# remove leading ../../
-# 		$incl =~ s/^[\.\/]{1,}//;
-# 		for (my $i = 0;; ++$i) {
-# 			$incl = "../$incl";
-# 			if (-e $incl) {
-# 				printf "%s %s\n", $1, $incl;
-# 				$ln =~ s/"$1"/"$incl"/;
-# 				print $fp $ln;
-# 				last;
-# 			}
-# 			if ($i == 10) {
-# 				print "Couldn't find correct path for $1 in file $fnam\n";
-# 				last;
-# 			}
-# 		}
-# 	}
-# 	close($fp);
-# }
+##!/usr/bin/perl
+#use strict;
+#use warnings;
+
+#while (my $fnam = glob('*.c *.h *.cpp *.hpp')) {
+#	open(my $fp, '+<', $fnam) or die "Can't open $fnam\n";
+#	while (<$fp>) {
+#		# match file in #include file.cpp
+#		if (!/^[ \t]*#[ \t]*include[ \t\/]{1,}"([^"]{1,})"/) {
+#			next;
+#		}
+#		if (-e $1) {
+#			next;
+#		}
+#		my $incl = $1;
+#		# remove leading ../../
+#		$incl =~ s/^[\.\/]{1,}//;
+#		for (my $i = 0;; ++$i) {
+#			$incl = "../$incl";
+#			if (-e $incl) {
+#				s/"$1"/"$incl"/;
+#				print $fp $_;
+#				last;
+#			}
+#			if ($i == 10) {
+#				print "Couldn't find correct path for $1 in file $fnam\n";
+#				last;
+#			}
+#		}
+#	}
+#	close($fp);
+#}
