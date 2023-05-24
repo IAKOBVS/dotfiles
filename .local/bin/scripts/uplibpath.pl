605a6e39eca33a19297cf9e8fee5cfb54758a63c
James Tirta Halim <tirtajames45@gmail.com>
2023-05-24 19:32:26 +0700

    new

--- .local/bin/scripts/uplibpath.pl
+++ .local/bin/scripts/uplibpath.pl
@@ -7,23 +7,25 @@ foreach my $file (glob('*.c *.h *.cpp *.hpp')) {
 	open(my $old, '<', "$file.bak") or die "Can't open $file.bak\n";
 	open(my $new, '>', $file) or die "Can't open $file";
 	while (my $ln = <$old>) {
-		if ($ln =~ /^[ \t]*#[ \t]*include[ \t\/]{1,}"([^"]{1,})"/) {
-			my $oincl = $1;
-			if (!-e $oincl) {
-				my $incl = $oincl;
-				$incl =~ s/^[\/\.]{1,}//;
-				print "$incl\n";
-				for (my $max = 8; $max; $max--) {
-					$incl = "../$incl";
-					if (-e $incl) {
-						$ln =~ s/$oincl/$incl/;
-						print $ln;
-						print "Corrected $oincl to $incl in $file\n\n";
-						last;
-					}
-				}
+		if ($ln =~ !/^[ \t]*#[ \t]*include[ \t\/]{1,}"([^"]{1,})"/) {
+			goto PRINT_LINE;
+		}
+		my $oincl = $1;
+		if (-e $oincl) {
+			goto PRINT_LINE;
+		}
+		my $incl = $oincl;
+		$incl =~ s/^[\/\.]{1,}//;
+		print "$incl\n";
+		for (my $max = 8; $max; $max--) {
+			$incl = "../$incl";
+			if (-e $incl) {
+				$ln =~ s?$oincl?$incl?;
+				print "Corrected $oincl to $incl in $file\n\n";
+				last;
 			}
 		}
+PRINT_LINE:
 		print $new $ln;
 	}
 	close($old);
